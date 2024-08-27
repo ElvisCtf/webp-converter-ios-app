f6779e3baba8e20a47bc84d1b9c217609b6a8a56
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2024-08-27 17:49:07 +0800

    feat: basic UI of ConverterViewController

---
+++ ImageConverter/ConverterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConverterViewController.swift
+//  ImageConverter
+//
+//  Created by Elvis Cheng (ESD - Software Trainee, Digital Solutions) on 27/8/2024.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavBarTranslucent()
+        self.view.backgroundColor = .systemBackground
+        self.navigationItem.title = "Image Converter"
+    }
+
+}
+
