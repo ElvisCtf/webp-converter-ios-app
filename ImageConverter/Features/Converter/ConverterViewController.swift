35fcded4e43e14b586e59682a92f32061ed4f6db
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2025-03-03 14:31:42 +0800

    feat: edit button in bottom toolbar

--- ImageConverter/Features/Converter/ConverterViewController.swift
+++ ImageConverter/Features/Converter/ConverterViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 import PhotosUI
 
 class ConverterViewController: UIViewController {
-    private lazy var converterView = ConverterView(viewModel: viewModel)
+    private lazy var converterView = ConverterView(parentVC: self, viewModel: viewModel)
     private let viewModel = ConverterViewModel()
     
     override func viewDidLoad() {
@@ -25,47 +25,7 @@ class ConverterViewController: UIViewController {
     }
     
     private func initToolBar() {
-        toolbarItems = [
-            UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addImage)),
-            UIBarButtonItem(systemItem: .flexibleSpace),
-            UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(convertImage))
-        ]
+        toolbarItems = converterView.toolbarItems
         navigationController?.setToolbarHidden(false, animated: false)
-    }
-    
-    @objc func addImage() {
-        var config = PHPickerConfiguration()
-        config.selectionLimit = 0
-        config.selection = .ordered
-        config.filter = .images
-        let imagePicker = PHPickerViewController(configuration: config)
-        imagePicker.delegate = self
-        present(imagePicker, animated: true)
-    }
-    
-    @objc private func convertImage() {
-        viewModel.convertImages()
-    }
-}
-
-
-// MARK: - TableView Delegate
-extension ConverterViewController: PHPickerViewControllerDelegate {
-    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        picker.dismiss(animated: true)
-        var index = 0
-        for result in results {
-            let itemProvider = result.itemProvider
-            // Check image is WebP
-            if itemProvider.hasItemConformingToTypeIdentifier(UTType.webP.identifier) {
-                // Convert WebP image into data object
-                itemProvider.loadDataRepresentation(forTypeIdentifier: UTType.webP.identifier) { data, error in
-                    guard error == nil else { return }
-                    self.viewModel.imageTasks.append(ImageTaskModel(index: index, filename: itemProvider.suggestedName ?? "", inputData: data, outputFormat: .PNG))
-                    index+=1
-                    self.converterView.reloadTable()
-                }
-            }
-        }
     }
 }
